25cd2263d4e8e2f30fac386af51703424c18175b
Li <RLi8@mdanderson.org>
2023-05-01 12:59:58 -0500

    Added return values for Rdatas

--- R/SimDataSce.R
+++ R/SimDataSce.R
@@ -9,6 +9,8 @@
 #' @format A SingleCellExperiment object
 #'
 #' @keywords datasets
+#' 
+#' @return Simulation SingleCellExperiment object to examplify the usage of the method
 #'
 #' @examples
 #' data(SimDataSce)
